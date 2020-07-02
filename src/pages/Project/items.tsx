a675893d70b0cbf8e696196dc06679739ef1556e
Shakil Ahmed <shakil147258@gmail.com>
2020-07-02 16:30:17 +0600

    feat: add project screen shots

--- src/pages/Project/items.tsx
+++ src/pages/Project/items.tsx
@@ -1,28 +1,38 @@
-interface ItemType {
+export interface ItemType {
   name: string;
   description: string;
-  image: string;
+  image: Array<string>;
   link: string;
 }
 export const Items: ItemType[] = [
   {
     name: "Pocketmoney",
-    description: "Fintech solution on Bangladeshi perspective",
-    image: "https://i.ytimg.com/vi/JCn7Qps1CqE/maxresdefault.jpg",
+    description:
+      "Pocket Money is solving the fundamental problem of not having simple, convenient and transparent access to credit in today’s finance.",
+    image: [
+      require("../../images/projects/pocketmoney/registration.jpg"),
+      require("../../images/projects/pocketmoney/customers.jpg"),
+      require("../../images/projects/pocketmoney/dashboard.jpg"),
+      require("../../images/projects/pocketmoney/homepage.jpg"),
+    ],
     link: "https://www.pocketmoney.com.bd/",
   },
   {
     name: "Jantrac",
     description: "Cleaning service provider",
-    image:
-      "https://www.skg.net.au/wp-content/uploads/2018/03/commercial-cleaning-services.jpg",
+    image: [
+      require("../../images/projects/jantrac/login.jpg"),
+      require("../../images/projects/jantrac/homepage.jpg"),
+    ],
     link: "https://www.jantrac.com/",
   },
   {
     name: "Q-Study",
     description: "Online School platform",
-    image:
-      "https://www.skg.net.au/wp-content/uploads/2018/03/commercial-cleaning-services.jpg",
+    image: [
+      require("../../images/projects/qstudy/homepage.jpg"),
+      require("../../images/projects/qstudy/registration.jpg"),
+    ],
     link: "https://www.jantrac.com/",
   },
 ];
