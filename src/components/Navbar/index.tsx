ab7ee2dddba871a1b94515c04c850771183d92d1
Shakil Ahmed <shakil147258@gmail.com>
2020-07-07 20:09:07 +0600

    feat: make sidebar fixed

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,100 +1,184 @@
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import {
-  AppBar,
-  Avatar,
-  Box,
   createStyles,
-  Divider,
-  IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
   makeStyles,
   Theme,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
-import Drawer from "@material-ui/core/Drawer";
+  useTheme,
+} from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState } from "react";
+import clsx from "clsx";
+import React from "react";
 import { Link } from "react-router-dom";
-import avatar from "../../images/sidebar-image.jpeg";
 import { SidebarItems } from "./sidebarItems";
-import "./styles.css";
-export const Navbar = () => {
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      drawer: {
-        width: 250,
-        backgroundColor: "#8ae9b3",
-        backgroundImage: "linear-gradient(315deg, #8ae9b3 0%, #c8d6e5 74%)",
 
-        height: "100%",
+const drawerWidth = 200;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      backgroundColor: "#222",
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+      color: "tomato",
+    },
+    hide: {
+      display: "none",
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: "nowrap",
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      backgroundColor: "#7f8c8d",
+      backgroundImage: "linear-gradient(315deg, #7f8c8d 0%, #000000 74%)",
+    },
+    drawerClose: {
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: "hidden",
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9) + 1,
       },
-      avatar: {
-        display: "block",
-        margin: "0.5rem auto",
-        width: theme.spacing(13),
-        height: theme.spacing(13),
-      },
-      listItem: {
-        color: "black",
-      },
-    })
-  );
+      backgroundColor: "#7f8c8d",
+      backgroundImage: "linear-gradient(315deg, #7f8c8d 0%, #000000 74%)",
+    },
+    toolbar: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+    avatar: {
+      display: "block",
+      margin: "0.5rem auto",
+      width: theme.spacing(13),
+      height: theme.spacing(13),
+    },
+  })
+);
+
+export const Navbar = (routes: any) => {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-  const [drawerPosition, setDrawerPosition] = useState({
-    left: false,
-  });
-
-  const toggleDrawer = () => {
-    setDrawerPosition({
-      ...drawerPosition,
-      left: !drawerPosition.left,
-    });
+  const handleDrawerOpen = () => {
+    setOpen(true);
   };
 
-  const drawerItems = (
-    <Box component="div" className={classes.drawer}>
-      <Avatar className={classes.avatar} src={avatar} alt="no image" />
-      <Divider />
-      <List component="nav">
-        {SidebarItems.map((item, index) => (
-          <ListItem
-            key={index}
-            className={classes.listItem}
-            button
-            component={Link}
-            {...{ to: item.route }}
-          >
-            <ListItemIcon>{item.itemIcon}</ListItemIcon>
-            <ListItemText primary={item.itemName} />
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <>
-      <Box component="nav">
-        <AppBar className="navBar" position="static">
-          <Toolbar>
-            <IconButton onClick={toggleDrawer}>
-              <MenuIcon className="navBarArrow" />
-            </IconButton>
-            <Typography variant="h6">Shakil Ahmed</Typography>
-            <Drawer
-              anchor="left"
-              open={drawerPosition.left}
-              onClose={toggleDrawer}
-            >
-              {drawerItems}
-            </Drawer>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Shakil Ahmed
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon style={{ color: "tomato" }} />
+            ) : (
+              <ChevronLeftIcon style={{ color: "tomato" }} />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {SidebarItems.map((item, index) => (
+            <ListItem button key={index} component={Link} to={item.route}>
+              <ListItemIcon style={{ color: "tomato" }}>
+                {item.itemIcon}
+              </ListItemIcon>
+              <ListItemText
+                style={{ color: "white" }}
+                primary={item.itemName}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {routes.children}
+      </main>
+    </div>
   );
 };
