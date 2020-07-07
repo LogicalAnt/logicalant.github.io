fb33969359fea9bbb514540981249b165bc1244b
Shakil Ahmed <shakil147258@gmail.com>
2020-07-07 20:08:31 +0600

    feat: make nabvar available for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 import { About } from "./pages/About";
 import { Blog } from "./pages/Blog";
 import { Contact } from "./pages/Contact";
@@ -13,16 +14,18 @@ import { Skill } from "./pages/Skill";
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/experience" component={Experience} />
-        <Route exact path="/project" component={Project} />
-        <Route exact path="/skill" component={Skill} />
-        <Route exact path="/education" component={Education} />
-        <Route exact path="/blog" component={Blog} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/" component={Home} />
-      </Switch>
+      <Navbar>
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/experience" component={Experience} />
+          <Route exact path="/project" component={Project} />
+          <Route exact path="/skill" component={Skill} />
+          <Route exact path="/education" component={Education} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Navbar>
     </BrowserRouter>
   );
 }
