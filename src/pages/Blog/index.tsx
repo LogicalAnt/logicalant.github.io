da309f11f36f913550c10f2bfbd9bd4b013ab2a1
Shakil Ahmed <shakil147258@gmail.com>
2020-07-08 14:49:49 +0600

    feat: add bolg component

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,5 +1,57 @@
+import { Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
+import { Items } from "./items";
+import "./style.css";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+    cardHolder: {
+      margin: 25,
+      "&:hover": {
+        boxShadow: "3px 3px 30px rgba(61,255,0,0.6)",
+      },
+    },
+    media: {
+      height: 140,
+    },
+  })
+);
 
 export const Blog = () => {
-  return <>Blogs</>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <Grid container className={classes.root}>
+        {Items.map((post, index) => (
+          <Grid item sm={12} xs={12} md={6} lg={4} key={index}>
+            <Card className={`${classes.cardHolder} cardHolder`}>
+              <CardActionArea target="blank" href={post.link}>
+                <CardMedia className={classes.media} image={post.image[0]} />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="h6">
+                    {post.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {post.description.substring(0, 35)}...
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 };
---
+++ src/pages/Blog/items.tsx
@@ -0,0 +1,24 @@
+export interface ItemType {
+  name: string;
+  description: string;
+  image: Array<string>;
+  link: string;
+}
+export const Items: ItemType[] = [
+  {
+    name: "ইন্টারফেস (অবজেক্ট ওরিয়েন্টেড প্রোগ্রামিং)",
+    description:
+      "পিএইচপি তে ইন্টারফেস কাজ করে অনেকটা এ্যাডাপটারের মত। আমাদের ল্যাপটপের অ্যাডাপটারের কাজ কি ?",
+    image: ["https://miro.medium.com/max/1400/1*vVXicFhmOOyYASuzkM570w.jpeg"],
+    link:
+      "https://medium.com/%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A7%8B%E0%A6%97%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%AE%E0%A6%BF%E0%A6%82-%E0%A6%AA%E0%A6%BE%E0%A6%A4%E0%A6%BE/%E0%A6%87%E0%A6%A8%E0%A7%8D%E0%A6%9F%E0%A6%BE%E0%A6%B0%E0%A6%AB%E0%A7%87%E0%A6%B8-%E0%A6%85%E0%A6%AC%E0%A6%9C%E0%A7%87%E0%A6%95%E0%A7%8D%E0%A6%9F-%E0%A6%93%E0%A6%B0%E0%A6%BF%E0%A7%9F%E0%A7%87%E0%A6%A8%E0%A7%8D%E0%A6%9F%E0%A7%87%E0%A6%A1-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A7%8B%E0%A6%97%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%AE%E0%A6%BF%E0%A6%82-ef196691bd7f",
+  },
+  {
+    name: "প্রাইম জেনারেটর (সিভ অফ এরাটোস্হেনেস)",
+    description:
+      "একটা নির্দিষ্ট রেঞ্জের মধ্যের সব প্রাইম নাম্বার(মৌলিক সংখ্যা) বের করার জন্য সবচেয়ে প্রাচীন এবং ইফিশিনেন্ট পদ্ধতি এটি।",
+    image: ["https://miro.medium.com/max/4800/1*BTSuHDOFdD-J2qOW16WHfA.jpeg"],
+    link:
+      "https://medium.com/%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A7%8B%E0%A6%97%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%AE%E0%A6%BF%E0%A6%82-%E0%A6%AA%E0%A6%BE%E0%A6%A4%E0%A6%BE/%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%87%E0%A6%AE-%E0%A6%9C%E0%A7%87%E0%A6%A8%E0%A6%BE%E0%A6%B0%E0%A7%87%E0%A6%9F%E0%A6%B0-%E0%A6%B8%E0%A6%BF%E0%A6%AD-%E0%A6%85%E0%A6%AB-%E0%A6%8F%E0%A6%B0%E0%A6%BE%E0%A6%9F%E0%A7%8B%E0%A6%B8%E0%A7%8D%E0%A6%B9%E0%A7%87%E0%A6%A8%E0%A7%87%E0%A6%B8-3ba4bb81f041",
+  },
+];
