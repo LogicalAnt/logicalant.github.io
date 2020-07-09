1260b6083ca7b0db460ec6fe370a3e60510ac6a8
Shakil Ahmed <shakil147258@gmail.com>
2020-07-09 15:52:54 +0600

    feat: add social media address

--- src/pages/Home/content.tsx
+++ src/pages/Home/content.tsx
@@ -3,13 +3,19 @@ import {
   Box,
   createStyles,
   Grid,
+  IconButton,
   makeStyles,
   Theme,
+  Tooltip,
   Typography,
 } from "@material-ui/core";
+import EmailIcon from "@material-ui/icons/Email";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import React from "react";
 import Typed from "react-typed";
 import avatar from "../../images/sidebar-image.jpeg";
+import "./style.css";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -35,6 +41,15 @@ const useStyles = makeStyles((theme: Theme) =>
       color: "yellow",
       fontWeight: 500,
     },
+    facebookButton: {
+      color: "#4267B2",
+    },
+    linkedinButton: {
+      color: "#0C77B5",
+    },
+    emailButton: {
+      color: "#BC473C",
+    },
   })
 );
 export const HomeContent = () => {
@@ -45,22 +60,66 @@ export const HomeContent = () => {
       <Box className={classes.container}>
         <Grid container justify="center">
           <Avatar src={avatar} className={classes.avatar} />
+
+          <Grid item xs={12}>
+            <Typography variant="h4" className={classes.typedName}>
+              <Typed strings={["Shakil Ahmed"]} typeSpeed={100} />
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography className={classes.typedSkill}>
+              <Typed
+                strings={[
+                  "FULL-STACK SOFTWARE ENGINEER",
+                  "Javascript Enthusiast",
+                  "Php Adorer",
+                  "A Music Devotee",
+                ]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              />
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Box component="span" m={1}>
+              <IconButton
+                className={`${classes.emailButton} contactButton`}
+                href="mailto:shakil147258@gmail.com"
+                target="_top"
+              >
+                <Tooltip
+                  title="shakil147258@gmail.com"
+                  placement="bottom"
+                  arrow
+                >
+                  <EmailIcon fontSize="large" />
+                </Tooltip>
+              </IconButton>
+
+              <IconButton
+                className={`${classes.facebookButton} contactButton`}
+                href="https://www.facebook.com/shakil147258"
+                target="blank"
+              >
+                <Tooltip title="shakil147258" placement="bottom" arrow>
+                  <FacebookIcon fontSize="large" />
+                </Tooltip>
+              </IconButton>
+              <IconButton
+                className={`${classes.linkedinButton} contactButton`}
+                href="https://www.linkedin.com/in/unreleased/"
+                target="blank"
+              >
+                <Tooltip title="unreleased" placement="bottom" arrow>
+                  <LinkedInIcon fontSize="large" />
+                </Tooltip>
+              </IconButton>
+            </Box>
+          </Grid>
         </Grid>
-        <Typography variant="h4" className={classes.typedName}>
-          <Typed strings={["Shakil Ahmed"]} typeSpeed={100} />
-        </Typography>
-        <Typography className={classes.typedSkill}>
-          <Typed
-            strings={[
-              "FULL-STACK SOFTWARE ENGINEER",
-              "Javascript Enthusiast",
-              "Php Specialist",
-            ]}
-            typeSpeed={40}
-            backSpeed={50}
-            loop
-          />
-        </Typography>
       </Box>
     </>
   );
