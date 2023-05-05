5e57343ff2cc2d42661bdabbf4573af44ea04d7c
LogicalAnt <shakil147258@gmail.com>
2023-05-06 01:53:21 +0600

    refactor:

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -1,17 +1,17 @@
-import { Chip, Grid } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
-import React, { useState } from "react";
-import { Items, ItemType, tagColor } from "./items";
-import { ProjectDetailsModal } from "./modal";
-import "./style.css";
+import { Chip, Grid } from "@material-ui/core"
+import Button from "@material-ui/core/Button"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
+import React, { useState } from "react"
+import { Items, ItemType, tagColor } from "./items"
+import { ProjectDetailsModal } from "./modal"
+import "./style.css"
 
 export const Project = () => {
   const useStyles = makeStyles((theme: Theme) =>
@@ -36,19 +36,19 @@ export const Project = () => {
         height: 140,
       },
     })
-  );
+  )
 
-  const [hoverTagColor, setHoverTagColor] = useState("tomato");
-  const classes = useStyles();
+  const [hoverTagColor, setHoverTagColor] = useState("tomato")
+  const classes = useStyles()
 
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false)
   const [modalData, setModalData] = useState<ItemType>({
     description: "",
     tag: [""],
     image: [""],
     link: "",
     name: "",
-  });
+  })
 
   return (
     <>
@@ -92,8 +92,8 @@ export const Project = () => {
                   color="primary"
                   size="small"
                   onClick={() => {
-                    setModalOpen(true);
-                    setModalData(project);
+                    setModalOpen(true)
+                    setModalData(project)
                   }}
                   startIcon={<InfoOutlinedIcon />}
                 >
@@ -111,5 +111,5 @@ export const Project = () => {
         modalData={modalData}
       />
     </>
-  );
-};
+  )
+}
