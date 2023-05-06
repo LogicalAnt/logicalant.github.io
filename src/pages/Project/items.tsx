179137786f8047f506970fc49ae0dee0cd65a67f
LogicalAnt <shakil147258@gmail.com>
2023-05-06 19:34:30 +0600

    feat: :sparkles: add contra project resources

--- src/pages/Project/items.tsx
+++ src/pages/Project/items.tsx
@@ -5,13 +5,31 @@ export interface ItemType {
   image: Array<string>
   link: string
 }
+
 export const Items: ItemType[] = [
   {
     name: "Contra Mobile App",
     tag: ["react-native", "typescript", "expo"],
     description:
       "Contra is a mobile application build in react-native. Which is a communication app for real-estate site Contractor and workers",
-    image: [require("../../images/projects/pocketmoney/registration.jpg")],
+    image: [
+      require("../../images/projects/contra/login.jpg"),
+      require("../../images/projects/contra/chat-list.jpg"),
+      require("../../images/projects/contra/chat-member-add.jpg"),
+      require("../../images/projects/contra/chat-screen.jpg"),
+      require("../../images/projects/contra/different-sized-chat-image.jpg"),
+      require("../../images/projects/contra/profile-edit.jpg"),
+      require("../../images/projects/contra/prefecture-selection.jpg"),
+      require("../../images/projects/contra/map.jpg"),
+      require("../../images/projects/contra/improvement-request-screen.jpg"),
+      require("../../images/projects/contra/site-list.jpg"),
+      require("../../images/projects/contra/site-details.jpg"),
+      require("../../images/projects/contra/site-chat.jpg"),
+      require("../../images/projects/contra/site-photo-add-1.jpg"),
+      require("../../images/projects/contra/site-photo-add-2.jpg"),
+      require("../../images/projects/contra/site-photo-folder.jpg"),
+      require("../../images/projects/contra/photo-inside-site-folder.jpg"),
+    ],
     link: "https://play.google.com/store/apps/details?id=cx.vte.contra&hl=en_GB&gl=US",
   },
   {
@@ -67,7 +85,12 @@ export const tagColor = (tag: string) => {
       return "#0A758F"
     case "nodejs":
       return "#5FB047"
-
+    case "react-native":
+      return "#591ed94a"
+    case "typescript":
+      return "#d9881e6d"
+    case "expo":
+      return "#12a52549"
     default:
       return "tomato"
   }
