c23c3895e9e8c31c6ad17cadeb0f3484df42b2ef
LogicalAnt <shakil147258@gmail.com>
2023-05-07 21:34:09 +0600

    feat: :sparkles: reword some of the experience item

--- src/pages/Experience/items.tsx
+++ src/pages/Experience/items.tsx
@@ -34,7 +34,7 @@ export const Items: ItemType[] = [
     description: `
     Sheba.xyz is a largest service marketplace & one-stop solution for home services in Bangladesh.
     
-    Shorol Ltd. has suddenly stopped their operations due to Covid situation then I moved to Sheba.xyz
+    After Shorol Ltd. has suddenly stopped their operations due to Covid situation, I moved to Sheba.xyz
     Though I stayed here for a short period of time, but I really enjoyed the culture of their tech team`,
     startAt: moment("2020/08/01").format("MMMM Do YYYY"),
     endAt: moment("2020/09/01").format("MMMM Do YYYY"),
@@ -45,9 +45,9 @@ export const Items: ItemType[] = [
     role: "Software engineer",
     description: `After RS period, I joined Shorol Limited as a software engineer. 
       
-      It was a startup on fintech product. Shorol has its core product named Pocketmoney. Its main customer base was mostly the people who are below general banking credit score.
+      It was a fintech startup. Shorol has its core product named Pocketmoney. Its main customer base was mostly the people who are below general banking credit score.
        
-      I acted as Pocketmoney's one of a core developer. The product backend api's written in nodejs and react frontend. Using graphql makes it more robust application.
+      I acted as Pocketmoney's one of a core developer. The product's backend APIs written in nodejs and react frontend. Using graphql makes it more robust application.
       
       Working with Shorol was a great experience for me. That frindly and flexible work environment was really a pleasant moment`,
     startAt: moment("2019/05/01").format("MMMM Do YYYY"),
@@ -58,7 +58,7 @@ export const Items: ItemType[] = [
     companyName: "RS Software",
     role: "Web developer",
     description: `After my graduation I joined RS Software. It was a major breakthrough in my career. I joined here as a php web developer. It was a freelancing based company, so I had to engage with several client project. As a first job I've learned a lot about job culture in Bangladeshi perspective.
-    As for technical point of view We mostly stick to php framework codeigniter and mysql database
+    As for technical point of view we mostly stick to php framework codeigniter and mysql database
     `,
     startAt: moment("2017/11/01").format("MMMM Do YYYY"),
     endAt: moment("2019/05/01").format("MMMM Do YYYY"),
